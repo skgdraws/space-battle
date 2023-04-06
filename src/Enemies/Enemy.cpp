a03db43538c5bd2206dfeaef6655d506d4ac770d
jose <joloria@estudiantec.cr>
2023-04-06 11:23:32 -0600

    Set stats of the Enemy class

---
+++ src/Enemies/Enemy.cpp
@@ -0,0 +1,27 @@
+//
+// Created by jose on 4/5/23.
+//
+
+#include "Enemy.h"
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+
+using namespace sf;
+using namespace std;
+
+void Enemy::setHp() {
+
+}
+
+void Enemy::setSpeed() {
+
+}
+
+void Enemy::setColor() {
+
+}
+
+void Enemy::setDimensions() {
+
+}
---
+++ src/Enemies/Enemy.h
@@ -0,0 +1,37 @@
+//
+// Created by jose on 4/5/23.
+//
+
+#ifndef SPACE_BATTLE_ENEMY_H
+#define SPACE_BATTLE_ENEMY_H
+
+#include "SFML/Graphics.hpp"
+#include "SFML/System.hpp"
+/*
+ * Abstract Class
+ */
+class Enemy {
+protected:
+    // Display
+    sf::RenderWindow window;
+
+    sf::RectangleShape enemy;
+
+    // Stats
+    int hp;
+    int speed;
+    int posY;
+    int spawnTimer = 0;
+
+public:
+    void setHp();
+    void setSpeed();
+    void setColor();
+    void setDimensions();
+
+
+
+};
+
+
+#endif //SPACE_BATTLE_ENEMY_H
