f887cd17e10cf7355ab09601d6bfe35c2c4e8531
jose <joloria@estudiantec.cr>
2023-04-09 21:19:00 -0600

    I forgot the spawnTimer

--- src/DTO/Wave.cpp
+++ src/DTO/Wave.cpp
@@ -5,12 +5,13 @@
 #include "Wave.h"
 
 Wave::Wave() {// For now, when the difficulty sets in, rewrite the constructor.
+    this->spawnTimer = 30;
+
     this->waves.push_back(30);
     this->waves.push_back(35);
     this->waves.push_back(40);
     this->waves.push_back(45);
     this->waves.push_back(50);
-
 }
 
 void Wave::display() {
